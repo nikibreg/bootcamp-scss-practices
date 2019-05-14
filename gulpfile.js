c89fa58df21ccdb8f655ea18ab7b887f4e805a93
nikibreg <nbregv@yahoo.co.uk>
2019-05-14 20:14:11 +0400

    feat(SOURCE): Resolve additional conflicts

--- gulpfile.js
+++ gulpfile.js
@@ -35,14 +35,11 @@ gulp.task('html', function(){
       .pipe(gulp.dest('dist/'));
 });
 
-<<<<<<< HEAD
-=======
 gulp.task('images', function() {
     return gulp.src('source/images/**/*.*')
         .pipe(gulp.dest('dist/images/'));
 });
 
->>>>>>> 0de23440822fa6572896dbeba008861527a08357
 gulp.task('watch', function() {
     gulp.watch('source/*.html', (done) => {
         gulp.series(['clean:html', 'html'])(done);
@@ -50,11 +47,6 @@ gulp.task('watch', function() {
     gulp.watch('source/styles/**/*.scss', (done) => {
         gulp.series(['clean:css', 'sass'])(done);
     });
-<<<<<<< HEAD
-});
-
-const init = ['clean:css', 'sass', 'clean:html', 'html']
-=======
     gulp.watch('source/images/**/*.*', (done) => {
         gulp.series(['clean:images', 'images'])(done);
     });
@@ -68,6 +60,5 @@ const init = [
     'clean:images',
     'images'
 ];
->>>>>>> 0de23440822fa6572896dbeba008861527a08357
 
 gulp.task('default', gulp.series([...init,'watch']));
